40385a0186ddb807b647ee5c05919c833351cdd2
Chance Smith <chancesmith97@hotmail.com>
2020-10-03 23:12:44 +0000

    Automatically backed up by Learn

--- src/components/Game.js
+++ src/components/Game.js
@@ -20,7 +20,7 @@ class Game extends React.Component {
   componentDidMount() {
     this.time = setCurrentTime();
   }
-  
+
   setCurrentTime = () => {
     this.setState({ time: new Date(Date.now())});
   }
